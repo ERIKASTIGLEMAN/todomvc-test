3cbbc3a5640b3a6a138ccf3dfceafffca4f18587
Erika Stigleman <Erika.Stigleman@outlook.com>
2021-10-31 15:37:52 -0400

    Adding a timeout option to the get allowing cypress to wait for objects

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress"/>
 
 it("should be able to add a new todo to the list", () => {
-	cy.visit("http://todomvc-app-for-testing.surge.sh/?delay-new-todo=3000");
+	cy.visit("http://todomvc-app-for-testing.surge.sh/?delay-new-todo=5000");
 
-	cy.get(".new-todo").type("Clean Room {enter}");
+	cy.get(".new-todo", { timeout: 6000 }).type("Clean Room {enter}");
 });
