a62c33c2f846d9f7a3d0e9e744a078be887bc2fd
Erika Stigleman <Erika.Stigleman@outlook.com>
2021-10-31 16:46:52 -0400

    Validation of text Clean Room

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -4,7 +4,10 @@ it("should be able to add a new todo to the list", () => {
 	cy.visit("http://todomvc-app-for-testing.surge.sh/");
 
 	cy.get(".new-todo", { timeout: 6000 }).type("Clean Room {enter}");
-	cy.get(".toggle").click();
 
-	cy.contains(".clear-completed").click();
+	cy.get("label").should("have.text", "Clean Room");
+
+	//	cy.get(".toggle").click();
+
+	//	cy.contains("Clear completed").click();
 });
