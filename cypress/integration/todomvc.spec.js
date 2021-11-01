644989de2940a7573303218516812c529975b5e8
Erika Stigleman <Erika.Stigleman@outlook.com>
2021-11-01 10:36:39 -0400

    describe group - todo actions

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,26 +1,26 @@
 /// <reference types="cypress"/>
 
-it("should be able to add a new todo to the list", () => {
-	cy.visit("http://todomvc-app-for-testing.surge.sh/");
-
-	cy.get(".new-todo", { timeout: 6000 }).type("Clean Room {enter}");
-
-	cy.get("label").should("have.text", "Clean Room");
-
-	cy.get(".toggle").should("not.be.checked");
-
-	cy.get(".toggle").click();
-	cy.get("label").should("have.css", "text-decoration-line", "line-through");
-
-	cy.contains("Clear completed").click();
-
-	cy.get(".todo-list").should("not.have.descendants", "li");
-});
-
 //create 3 test to separate the add todo, toggle tet, and clear testing (they are all encapsulated in the it() function above)
 
-it("should add a new item to the list", () => {});
+describe("todo actions", () => {
+	it("should add a new item to the list", () => {
+		cy.visit("http://todomvc-app-for-testing.surge.sh/");
 
-it("should mark a todo completed", () => {});
+		cy.get(".new-todo", { timeout: 6000 }).type("Clean Room {enter}");
 
-it("should clear completed todos", () => {});
+		cy.get("label").should("have.text", "Clean Room");
+
+		cy.get(".toggle").should("not.be.checked");
+	});
+
+	it("should mark a todo completed", () => {
+		cy.get(".toggle").click();
+		cy.get("label").should("have.css", "text-decoration-line", "line-through");
+	});
+
+	it("should clear completed todos", () => {
+		cy.contains("Clear completed").click();
+
+		cy.get(".todo-list").should("not.have.descendants", "li");
+	});
+});
