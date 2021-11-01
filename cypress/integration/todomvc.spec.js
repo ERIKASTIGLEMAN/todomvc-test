9cb7053c3b668cf9d6c40b57499e587e570469ea
Erika Stigleman <Erika.Stigleman@outlook.com>
2021-11-01 10:51:25 -0400

    beforeEach making the test independent

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,13 +1,17 @@
 /// <reference types="cypress"/>
 
+const { beforeEach } = require("mocha");
+
 //create 3 test to separate the add todo, toggle tet, and clear testing (they are all encapsulated in the it() function above)
 
 describe("todo actions", () => {
-	it.only("should add a new item to the list", () => {
+	beforeEach(() => {
 		cy.visit("http://todomvc-app-for-testing.surge.sh/");
 
-		cy.get(".new-todo", { timeout: 6000 }).type("Clean Room {enter}");
+		cy.get(".new-todo").type("Clean Room {enter}");
+	});
 
+	it.only("should add a new item to the list", () => {
 		cy.get("label").should("have.text", "Clean Room");
 
 		cy.get(".toggle").should("not.be.checked");
