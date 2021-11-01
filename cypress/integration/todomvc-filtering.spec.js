af822bc238836bf584c064f0a21f590a4bd8ef87
Erika Stigleman <Erika.Stigleman@outlook.com>
2021-11-01 11:34:35 -0400

    adding selector to target specific item in todo list

---
+++ cypress/integration/todomvc-filtering.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress"/>
+
+describe("filtering", () => {
+	beforeEach(() => {
+		cy.visit("http://todomvc-app-for-testing.surge.sh/");
+
+		cy.get(".new-todo").type("Clean Room {enter}");
+		cy.get(".new-todo").type("Study selectors {enter}");
+		cy.get(".new-todo").type("Learn cypress {enter}");
+
+		cy, get(".todo-list li:nth-child(2) .toggle").click();
+	});
+});
